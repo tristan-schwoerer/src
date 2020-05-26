a879ab7cf24ff5ae7bebeba1d78c6b5a331c4ade
Tristan <tristan@5schwoerer.de>
2020-05-26 10:15:51 +0200

    hough trafo first try

---
+++ hough_trafo/src/listener.cpp
@@ -0,0 +1,18 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/LaserScan.h"
+#include "visualization_msgs/Marker.h"
+
+void chatterCallback(const sensor_msgs::LaserScan& msg)
+	{
+	ROS_INFO("LaserData: [%f]",msg.ranges[360]);
+	}
+
+int main(int argc, char **argv)
+	{
+		ros::init(argc, argv, "listener");
+		ros::NodeHandle n;
+		ros::Subscriber sub = n.subscribe("scan", 1000, chatterCallback);
+		ros::spin();
+		return 0;
+	}
